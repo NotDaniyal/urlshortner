2ea789c8ce2ec76a676abdb5f4478cdace5852f5
NotDaniyal <xmr121314@gmail.com>
2025-01-01 12:49:58 +0530

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,14 +3,16 @@ import Link from 'next/link'
 const Navbar = () => {
   return (
     <nav className='h-16 bg-cyan-700 flex justify-between px-3 items-center text-white'>
-      <div className="logo font-bold text-lg">shortLinks</div>
+      <div className="logo font-bold text-2xl">
+      <Link href="/"><li>ShortLinks</li></Link>
+      </div>
         <ul className='flex justify-center gap-4 items-center'>
          <Link href="/"><li>Home</li></Link>
          <Link href="/about"><li>About</li></Link>
-         <Link href="/generate"><li>Shortner</li></Link>
+         <Link href="/shorten"><li>Shortner</li></Link>
          <Link href="/contact"><li>Contact Us</li></Link>
          <li className="flex gap-3">
-          <Link href="/generate"><button className='bg-red-500 rounded-lg shadow-lg p-3 py-1 font-bold'>Try Now</button></Link>
+          <Link href="/shorten"><button className='bg-red-500 rounded-lg shadow-lg p-3 py-1 font-bold'>Try Now</button></Link>
           <Link href="/github"><button className='bg-red-500 rounded-lg shadow-lg p-3 py-1 font-bold'>GitHub</button></Link>
          </li>
         </ul>
