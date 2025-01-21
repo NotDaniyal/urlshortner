ba02291992d81e0821ff21f5286392508fa45fd1
Daniyal <xmr121314@gmail.com>
2025-01-21 10:41:55 +0530

    Add files via upload

---
+++ app/api/generate/route.js
@@ -0,0 +1,15 @@
+import clientPromise from "@/lib/mongodb";
+
+export async function POST(request) {
+    
+    const body = request.json()
+    const client = await clientPromise;
+    const db = client.db("bitlinks")
+    const collection = db.collection("url")
+
+    const result = await collection.insertOne({
+        url: body.url,
+        shorturl: body.shorturl
+    })
+    return Response.json({success:true, error:false, message: 'URL Generated successfully'})
+}
