996a5bb68c62a8d924b62b49f2be541fc8addb82
Daniyal <xmr121314@gmail.com>
2025-02-03 12:44:57 +0530

    Update route.js

--- app/api/generate/route.js
+++ app/api/generate/route.js
@@ -1,15 +1,20 @@
 import clientPromise from "@/lib/mongodb";
 
 export async function POST(request) {
-    
-    const body = request.json()
-    const client = await clientPromise;
-    const db = client.db("bitlinks")
-    const collection = db.collection("url")
+  
+  const body = await request.json()
+  const client = await clientPromise;
+  const db = client.db("bitlinks")
+  const collection = db.collection("url")
 
-    const result = await collection.insertOne({
-        url: body.url,
-        shorturl: body.shorturl
-    })
-    return Response.json({success:true, error:false, message: 'URL Generated successfully'})
+  const doc = await collection.findOne({shorturl: body.shorturl})
+  if(doc){
+    return Response.json({success: false, error: true, message: 'URL already exists!'})
+  }
+  const result = await collection.insertOne({
+    url: body.url,
+    shorturl: body.shorturl
+  })
+
+  return Response.json({success: true, error: false, message: 'URL Generated Successfully'})
 }
